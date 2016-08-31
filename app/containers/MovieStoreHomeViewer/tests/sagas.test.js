1e67bb2acdd71430702addba3c23892efe48a026
Amir Yasin <ayasin@datatensor.com>
2016-08-31 08:47:09 -0500

    cleanup

--- app/containers/MovieStoreHomeViewer/tests/sagas.test.js
+++ app/containers/MovieStoreHomeViewer/tests/sagas.test.js
@@ -1,14 +1,10 @@
-/* eslint-disable */
 /**
  * Test  sagas
  */
 
-import expect from 'expect';
-import { take, call, put, select } from 'redux-saga/effects';
-//import { defaultSaga } from '../sagas';
-
-import { getFacultyGradesForCourse } from '../../../api/GradeBookApi';
-import { newGradesArrived } from '../actions';
+// import expect from 'expect';
+// import { take, call, put, select } from 'redux-saga/effects';
+// import { defaultSaga } from '../sagas';
 
 /*
 const generator = defaultSaga();
